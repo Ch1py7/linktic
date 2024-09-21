6343397a20827f05922254cc58325780b52efea9
ch1py7 <garcialopezgerardo6612@gmail.com>
2024-09-21 01:13:59 -0700

    feat: add disabled option for button component

--- apps/frontend/src/components/button.svelte
+++ apps/frontend/src/components/button.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   export let style: string
-  export let type: "button" | "reset" | "submit" | null | undefined = "button"
+  export let type: 'button' | 'reset' | 'submit' | null | undefined = 'button'
+  export let disabled = false
 </script>
 
-<button type={type} on:click class={`${style} py-1 px-2 rounded-lg bottom-0`}>
+<button {disabled} {type} on:click class={`${style} py-1 px-2 rounded-lg bottom-0`}>
   <slot />
 </button>
