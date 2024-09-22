fdd11efe37f53b6bc9b824a711ef7e275242902d
ch1py7 <garcialopezgerardo6612@gmail.com>
2024-09-22 00:28:59 -0700

    fix: add secure protoco to url

--- apps/frontend/src/lib/http-client/axiosInstance.ts
+++ apps/frontend/src/lib/http-client/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-	baseURL: 'http://linktic.onrender.com/api',
+	baseURL: 'https://linktic.onrender.com/api',
 	headers: {
 		'Content-Type': 'application/json',
 	},
