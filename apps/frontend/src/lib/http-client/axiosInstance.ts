3940b3caacb30d8279206aef129f0fb236630973
ch1py7 <garcialopezgerardo6612@gmail.com>
2024-09-22 00:14:17 -0700

    fix: change back url

--- apps/frontend/src/lib/http-client/axiosInstance.ts
+++ apps/frontend/src/lib/http-client/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-	baseURL: 'http://localhost:8765/api',
+	baseURL: 'http://linktic.onrender.com/api',
 	headers: {
 		'Content-Type': 'application/json',
 	},
